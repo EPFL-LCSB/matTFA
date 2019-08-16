274e2112534ed7e36d754cf54881dfa58ec41c82
maria <maria.masidbarcon@epfl.ch>
2019-08-16 10:11:42 +0200

    ENH: Corrected version of the function to check transport rxns. The previous version was problematic with the lumps

--- matTFA/checkIfRxnIsTransport.m
+++ matTFA/checkIfRxnIsTransport.m
@@ -9,28 +9,39 @@ function isTrans = checkIfRxnIsTransport(reactants,products)
 % OUTPUTS
 % - isTrans: is a flag. 1 if the reaction is classified as transport
 %   reaction, and 0 otherwise.
-
+ 
 isTrans = 0;
 react={};
 prod={};
 reactants=columnVector(reactants);
 products=columnVector(products);
-
+ 
 for i=1:length(reactants)
     [reactname,reactComp]=parseMet(reactants{i,1});
     react{i,1}=reactname;
 end
-
+ 
 for i=1:length(products)
     [prodname,prodComp]=parseMet(products{i,1});
     prod{i,1}=prodname;
 end
-
 for i=1:length(reactants)
-    if ~isempty(find(ismember(prod,react(i))))
-        isTrans = 1;
+    [~, ba]=ismember(prod, react(i));
+    if length(find(ba))==1
+        isTrans=1;
+    elseif find(ba)>1
+        isTrans=0;
         return
     end
 end
-
+ 
+for i=1:length(products)
+    [~, ba]=ismember(react, prod(i));
+    if length(find(ba))==1
+        isTrans=1;
+    elseif find(ba)>1
+        isTrans=0;
+        return
+    end
+end
 end
