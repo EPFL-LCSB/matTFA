8f97bcea3a8fe4bd29070cdb52d1dc69d94e4e15
maria <maria.masidbarcon@epfl.ch>
2019-08-19 13:03:48 +0200

    ENH: this is the version of addDemandReaction used by redGEM! NOTE: Do we need to create a new function for this?

--- ext/Cobra205_vDec2014/reconstruction/addDemandReaction.m
+++ ext/Cobra205_vDec2014/reconstruction/addDemandReaction.m
@@ -1,4 +1,4 @@
-function [model,rxnNames] = addDemandReaction(model,metaboliteNameList)
+function [model,rxnNames] = addDemandReaction(model,metaboliteNameList,rev,lb,ub)
 % addDemandReaction adds demand reactions for a set of metabolites
 % The reaction names for the demand reactions will be DM_[metaboliteName]
 %
@@ -15,15 +15,34 @@ function [model,rxnNames] = addDemandReaction(model,metaboliteNameList)
 % Markus Herrgard 5/8/07
 % Ines Thiele 03/09 - Corrected reaction coefficient for demand reaction
 
+if nargin < 3
+    rev = false;
+    lb = 0;
+    ub = 1000;
+end
+
 if (~iscell(metaboliteNameList))
     tmp = metaboliteNameList;
     clear metaboliteNameList;
     metaboliteNameList{1} = tmp;
 end
 
+model.isDrain=zeros(length(model.rxns),1);
+
 for i = 1:length(metaboliteNameList)
-    rxnName = ['DM_' metaboliteNameList{i}];
+  
+    rxnName = ['DM_' strrep(metaboliteNameList{i},'-','_')];
     rxnNames{i}=rxnName;
     metaboliteList = {metaboliteNameList{i}};
-    model = addReaction(model,rxnName,metaboliteList,-1,false,0,1000,0,'Demand');
+    model = addReaction(model,rxnName,metaboliteList,-1,rev,lb,ub,0,'Demand');
+end
+
+model.isDrain=vertcat(model.isDrain,ones(length(metaboliteNameList),1));
+
+if isfield(model,'rxnOperators')
+    model.rxnOperators(length(model.rxnOperators)+1:length(model.rxns),1)=cellstr(repmat('NA',length(model.rxns)-length(model.rxnOperators),1));
+end
+
+if isfield(model,'rxnMapResult')
+    model.rxnMapResult(length(model.rxnMapResult)+1:length(model.rxns),1)=cellstr(repmat('drain flux',length(model.rxns)-length(model.rxnMapResult),1));
 end
